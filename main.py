81a37bb8336df16386d070829d1780f462cd5c80
roycehoe <royceinbox@hotmail.com>
2024-01-17 11:15:25 +0800

    feat: Create all prompts for calculator app

---
+++ main.py
@@ -0,0 +1,19 @@
+TEXT_INTRODUCTION = "Hi there! Thank you for using my calculator app"
+TEXT_EXIT = "Goodbye"
+
+PROMPT_INTRODUCTION = "Please press 1 to begin. Otherwise, press 0 to exit"
+PROMPT_INTRODUCTION_INVALID_INPUT = (
+    "You have entered an invalid prompt. Please try again"
+)
+
+PROMPT_FIRST_NUMBER = "Please input your first number"
+PROMPT_SECOND_NUMBER = "Please input your second number"
+PROMPT_MATHEMATICAL_OPERATION = """
+Please select your mathematical operation by inputing the corresponding number
+
+[1] - Addition
+[2] - Subtraction
+[3] - Multiplication
+[4] - Division
+"""
+PROMPT_INVALID_NUMBER = "You have entered an invalid number. Please try again"
