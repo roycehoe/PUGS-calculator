7464d39c75850fc97cb673f3e39a2643b919172a
roycehoe <royceinbox@hotmail.com>
2024-01-17 11:20:02 +0800

    feat: Create enums and exceptions to elucidate program states and errors

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from enum import Enum, auto
+
 TEXT_INTRODUCTION = "Hi there! Thank you for using my calculator app"
 TEXT_EXIT = "Goodbye"
 
@@ -6,8 +8,8 @@ PROMPT_INTRODUCTION_INVALID_INPUT = (
     "You have entered an invalid prompt. Please try again"
 )
 
-PROMPT_FIRST_NUMBER = "Please input your first number"
-PROMPT_SECOND_NUMBER = "Please input your second number"
+PROMPT_FIRST_NUMERICAL_CONSTANT = "Please input your first number"
+PROMPT_SECOND_NUMERICAL_CONSTANT = "Please input your second number"
 PROMPT_MATHEMATICAL_OPERATION = """
 Please select your mathematical operation by inputing the corresponding number
 
@@ -17,3 +19,24 @@ Please select your mathematical operation by inputing the corresponding number
 [4] - Division
 """
 PROMPT_INVALID_NUMBER = "You have entered an invalid number. Please try again"
+
+
+class MathematicalOperation(Enum):
+    ADDITION = auto()
+    SUBTRACTION = auto()
+    MULTIPLICATION = auto()
+    DIVISION = auto()
+
+
+class InvalidIntroductionInputError(Exception):
+    pass
+
+
+class InvalidMathematicalOperationError(Exception):
+    pass
+
+
+class InvalidNumericalConstantError(Exception):
+    pass
+
+
